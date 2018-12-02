2cba934cc10e0aa8e9d812738fe8f0afb8ee6560
Hejiadong <820873255@qq.com>
2018-12-02 21:12:58 +0800

    12.2 （bianrysearch recursive forgetful finished）

--- search.cpp
+++ search.cpp
@@ -20,14 +20,27 @@ void printv(const vector<T> &in){
 	cout<<endl;
 }
 template<typename T>
-int sequentialsearch(vector<T> const in,T key){
+int sequentialsearch(vector<T> const &in,T key){
 	int t;
 	for(t=in.size()-1;t>-1;t--){
 		if(in[t]==key)return t;
 	}
 	return -1;
 }
-
+template<typename T>
+int binarysearchrf(vector<T> const &in,int bot,int top,T key){
+	if(bot==top){
+		if(in[bot]==key)return bot;
+		return -1;
+	}
+	int mid=bot+(top-bot)/2;
+	if(in[mid]<key)return binarysearchrf(in,mid+1,top,key);
+	return binarysearchrf(in,bot,mid,key);
+}
+template<typename T>
+int binarysearch(vector<T> const &in,T key){
+	return binarysearchrf(in,0,in.size()-1,key);
+}
 
 int main(){
 	double key;
@@ -35,7 +48,8 @@ int main(){
 	createv(in);
 	cin>>key;
 	sort(in.begin(),in.end());
-	cout<<sequentialsearch(in,key)<<endl;
+	//cout<<sequentialsearch(in,key)<<endl;
+	cout<<binarysearch(in,key)<<endl;
 	return 0;
 }
 
