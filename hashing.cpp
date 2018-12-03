1a37202c8d29fcd8fa9983477a0d97762a7e551f
Hejiadong <820873255@qq.com>
2018-12-03 21:32:56 +0800

    12.3（radixsort finished）

---
+++ hashing.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+template<typename T>
+class hashtable1{
+	private:
+		vector<T> table;
+		int max;
+	public:
+		
+};
---
+++ radixsort.cpp
@@ -0,0 +1,63 @@
+#include<string>
+#include<queue>
+#include<list>
+#include<iostream>
+using namespace std;
+template<typename T>
+class sortlist:public list<T>{
+	public:
+		void radixsort();
+		void print(){
+			auto now=this->begin();
+			for(int t=0;t<this->size();t++){
+				cout<<*now<<" ";
+				now++;
+			}
+			cout<<endl;
+		}
+	private:
+		void assign(queue<T> *in);
+};
+int getpos(char in){
+	if(in==' ')return 0;
+	if(in>='a'&&in<='z')return in-'a'+1;
+	if(in>='A'&&in<='Z')return in-'A'+1;
+	return 27;
+}
+template<typename T>
+void sortlist<T>::radixsort(){
+	int size=3;
+	queue<T> all[28];
+	for(int t=size-1;t>-1;t--){
+		while(this->empty()==false){
+			T now=this->front();
+			this->pop_front();
+			int pos=getpos(now[t]);
+			all[pos].push(now);
+		}
+		assign(all);
+	}
+}
+template<typename T>
+void sortlist<T>::assign(queue<T> *in){
+	for(int t=0;t<28;t++){
+		while(in[t].empty()==false){
+			this->push_back(in[t].front());
+			in[t].pop();
+		}
+	}
+}
+int main(){
+	sortlist<string> test;
+	int n;
+	string temp;
+	cin>>n;
+	for(int t=0;t<n;t++){
+		cin>>temp;
+		test.push_back(temp);
+	}
+	test.print();
+	test.radixsort();
+	test.print();
+	return 0; 
+}
